7db67a54c42968ddbcf1bfb4bf0cfa9294f2a433
Sergey <bindasov.sergey@gmail.com>
2022-11-15 15:37:55 +0100

    init
    start

---
+++ TaskFour/TaskFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFourApp.swift
+//  TaskFour
+//
+//  Created by Sergey on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
