8329309100aefabe28722ae73ae6519dba24f37f
Sergey <bindasov.sergey@gmail.com>
2022-11-15 18:26:35 +0100

    на проверку

--- TaskFour/TaskThree.swift
+++ TaskFour/TaskThree.swift
@@ -26,39 +26,3 @@ struct TaskThree_Previews: PreviewProvider {
         TaskThree()
     }
 }
-
-
-
-//NavigationView {
-//    ZStack {
-//        Color.white
-//            .ignoresSafeArea()
-//        VStack {
-//            Image("logo")
-//                .resizable()
-//                .frame(width: 360, height: 70, alignment: .center)
-//                .padding()
-//            TextField("Username", text: $username)
-//                .padding()
-//                .frame(width: 360, height: 60)
-//                .background(Color.black.opacity(0.05))
-//                .cornerRadius(10)
-//                .border(.red, width: CGFloat(wrongUsername))
-//            SecureField("Password", text: $password)
-//                padding()
-//                .frame(width: 360, height: 60)
-//                .background(Color.black.opacity(0.05))
-//                .cornerRadius(10)
-//                .border(.red, width: CGFloat(wrongPassword))
-//        }
-////                    Button("Login") {
-////
-//        
-////                    .foregroundColor(.white)
-////
-////                    .frame(width: 360, height: 60)
-////                    .background(Color.blue)
-//        }
-//}
-////  .navigationBarHidden(true)
-//}
